7d57a81f8d6a67f752be9a903b0241a80c5681d9
sxc <qiuyequ@icloud.com>
2021-04-02 11:45:24 +0800

    Feature 4

--- GitTutorial/ContentView.swift
+++ GitTutorial/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
             .padding()
         
         // feature 2
-        // feature 2
+        // feature 3
+        // feature 4
             
         
         
