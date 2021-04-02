ecb9c9b1fc35449aaf5a352d5e2b1114388c5031
sxc <qiuyequ@icloud.com>
2021-04-02 11:19:34 +0800

    Initial Commit

---
+++ GitTutorial/GitTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTutorialApp.swift
+//  GitTutorial
+//
+//  Created by Xiaochun Shen on 2021/4/2.
+//
+
+import SwiftUI
+
+@main
+struct GitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
